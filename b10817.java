681ef04c0e9ec9d297f738cbc64b817a4849da93
jonghyeon <jhyeon0314@gmail.com>
2025-02-16 23:27:59 +0900

    solved:java, b10817

---
+++ b10817.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class b10817 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        int mid = (a >= b && a <= c) || (a <= b && a >= c) ? a :
+                (b >= a && b <= c) || (b <= a && b >= c) ? b : c;
+
+        bw.write(mid + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
